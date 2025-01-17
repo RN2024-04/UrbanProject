9f3b35b1fdd73e3de3b7e1bbff1e4d8a88192cc1
RN2024-04 <166221228+RN2024-04@users.noreply.github.com>
2025-01-17 23:42:30 +0500

    Update forms.py

--- detection_site/object_detection/forms.py
+++ detection_site/object_detection/forms.py
@@ -1,42 +1,22 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.core.exceptions import ValidationError
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
-from object_detection.models import PeopleReg
 
 
 class LoginForm (AuthenticationForm):
     username=forms.CharField()
     password = forms.CharField()
 
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)  # Добавляем поле email
 
-class UserRegisterForm (UserCreationForm):
-    username = forms.CharField()
-    password = forms.CharField()
-    password2 = forms.CharField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']  # Используем стандартные поля
 
     def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['password'],
-            self.cleaned_data["password2"]
-        )
+        user = super().save(commit=False)  # Получаем пользователя из родительского класса
+        user.email = self.cleaned_data['email']  # Сохраняем email
         if commit:
-            user.save()
+            user.save()  # Сохраняем пользователя
         return user
-
-
-# class RegisterForm (UserCreationForm):
-#     username=forms.CharField()
-#     password = forms.CharField()
-#     password2 = forms.CharField()
-#
-#     def save(self, commit=True):
-#         user = super(RegisterForm, self).save(commit=False)
-#         user.username = self.cleaned_data["username"]
-#         user.password = self.cleaned_data["password"]
-#         user.password2 = self.cleaned_data["password2"]
-#         if commit:
-#             user.save()
-#         return user
