62124e0e839fc9939417ebed9611322bc4d85458
RN2024-04 <166221228+RN2024-04@users.noreply.github.com>
2025-01-17 23:43:38 +0500

    Update views.py

--- detection_site/object_detection/views.py
+++ detection_site/object_detection/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm, UserRegisterForm
+from .forms import  UserRegisterForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def index2(request):
     return render(request, 'object_detection/home.html')
 
 
 def login1(request):
     if request.method=='POST':
-        form =LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
             # аутефикация
-            user=authenticate(request,username=username,password=password)
-            if user:
-                login1(request,user)
-                return redirect("http://127.0.0.1:8000/")
-    else:
-        form=LoginForm()
-    return render(request, 'object_detection/login.html',{'form': form})
+        user=authenticate(request,username=username,password=password)
+        if user:
+            login1(request,user)
+            return redirect("http://127.0.0.1:8000/")
+        else:
+            return render(request, 'object_detection/login.html',{'error': 'Неверные учетные данные'})
+    return render(request, 'object_detection/login.html')
 
 
 def register(request):
@@ -29,8 +28,17 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("http://127.0.0.1:8000/login/")
+            return redirect(reverse('user_login'))
     else:
         form = UserRegisterForm()
-    return render (request, "object_detection/register.html",{"form": form})
-    # return render(request,'object_detection/base.html')
+    return render(request, "object_detection/register.html", {"form": form})
+
+
+def dashboard(request):
+    return render(request, 'object_detection/dashboard.html')
+
+
+
+def logout1(request):
+    logout(request)
+    return redirect(reverse('user_login'))
